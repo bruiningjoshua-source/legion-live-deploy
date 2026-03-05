559c89db11db246cc3c89b84a27d8ae6702af978
Joshua Bruining <bruiningjoshua@gmail.com>
2026-03-05 10:26:48 +0000

    File changes

--- functions/generateAgoraToken.ts
+++ functions/generateAgoraToken.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 import { RtcTokenBuilder, RtcRole } from 'npm:agora-access-token@2.0.4';
 
 Deno.serve(async (req) => {
