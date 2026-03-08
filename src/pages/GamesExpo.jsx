5bbdf6d9f4f5834f24084cc3d1624554ae4d2c0c
Joshua Bruining <bruiningjoshua@gmail.com>
2026-03-08 12:31:37 +0000

    File changes

--- src/pages/GamesExpo.jsx
+++ src/pages/GamesExpo.jsx
@@ -297,7 +297,7 @@ export default function GamesExpo() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.1 }}
-            className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4"
+            className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-7 gap-4"
           >
             {GAMES.map((game, i) => (
               <motion.div
