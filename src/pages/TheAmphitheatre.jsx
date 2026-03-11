fd239c43bd88d76aad09309448b5708d360973bf
Joshua Bruining <bruiningjoshua@gmail.com>
2026-03-11 13:08:46 +0000

    File changes

--- src/pages/TheAmphitheatre.jsx
+++ src/pages/TheAmphitheatre.jsx
@@ -70,9 +70,15 @@ function ShortCard({ item }) {
         )}
 
         {/* More button */}
-        <button className="absolute top-1.5 right-1.5 w-6 h-6 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-          <MoreVertical className="w-3.5 h-3.5 text-white/70" />
-        </button>
+        <div className="absolute top-1.5 right-1.5 opacity-0 group-hover:opacity-100 transition-opacity">
+          <VideoContextMenu 
+            video={item}
+            onAddToWatchLater={handleAddToWatchLater}
+            onAddToPlaylist={handleAddToPlaylist}
+            onDownload={handleDownload}
+            onShare={handleShare}
+          />
+        </div>
       </div>
     </a>
   );
