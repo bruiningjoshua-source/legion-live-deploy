4baf8bbe5809a1b25f20d4c1ac64ae40918abe85
Joshua Bruining <bruiningjoshua@gmail.com>
2026-03-11 10:33:46 +0000

    File changes

--- src/components/wallet/CurrencyPackages.jsx
+++ src/components/wallet/CurrencyPackages.jsx
@@ -294,7 +294,7 @@ export default function CurrencyPackages({ onPurchase, isProcessing }) {
 
       {/* Package Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        {packages.map((pkg, index) => (
+        {visiblePackages.map((pkg, index) => (
           <motion.div
             key={pkg.id}
             initial={{ opacity: 0, y: 20 }}
