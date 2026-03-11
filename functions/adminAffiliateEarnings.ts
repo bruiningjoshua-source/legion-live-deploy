34f93f784afc04e63b043e3f2423a83b2377edc6
Joshua Bruining <bruiningjoshua@gmail.com>
2026-03-11 10:09:00 +0000

    File changes

--- functions/adminAffiliateEarnings.ts
+++ functions/adminAffiliateEarnings.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 const CEO_AFFILIATES = ['rankincadence@gmail.com'];
 const PLATFORM_CUT = 0.10; // 10% to platform
--- functions/chargebackHandler.ts
+++ functions/chargebackHandler.ts
@@ -1,7 +1,7 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
-import Stripe from 'npm:stripe@14.21.0';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
+import Stripe from 'npm:stripe@17.5.0';
 
-const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY'));
+const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY'), { apiVersion: '2024-12-18.acacia' });
 
 // Chargeback response strategies
 const CHARGEBACK_STRATEGIES = {
