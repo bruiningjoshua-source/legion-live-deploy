7ebff25da55f377c9ae1b55014ae1c3ec9dcf159
Joshua Bruining <bruiningjoshua@gmail.com>
2026-03-17 10:57:40 +0000

    File changes

--- src/components/shared/LoadingScreen.jsx
+++ src/components/shared/LoadingScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
