0749936be0c96560efea6e52207e9e588bb0b27c
Joshua Bruining <bruiningjoshua@gmail.com>
2026-05-02 05:59:08 +0000

    File changes

--- src/lib/supabase/supabaseCore.js
+++ src/lib/supabase/supabaseCore.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://almpdnbqgfnsqlcmbucj.supabase.co';
-const supabaseAnonKey = 'sb_publishable_Im4pnwdacqAzFyCvhVWYpw_-W5qRW1m';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsbXBkbmJxZ2Zuc3FsY21idWNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NjA4ODQsImV4cCI6MjA5MDAzNjg4NH0.XoZvfoxkaUE7sn5Yu0TEyUriUcjNK0iz66gWXQYfUTk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
